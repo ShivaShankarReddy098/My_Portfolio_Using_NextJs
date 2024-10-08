f1a90fd15929709e8b3b7c7eb05af44519e3272e
ShivaShankarReddy098 <shankarreddyshiva83@gmail.com>
2024-10-08 16:56:34 +0530

    Added Animations to Cards

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -5,34 +5,35 @@ import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
 import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
 import { SectionHeader } from "@/components/SectionHeader";
 import Image from "next/image";
+import { Fragment } from "react";
 
 const testimonials = [
   {
-    name: "Abhishek Reddy",
+    name: "C.Ronaldo",
     position: "Marketing Manager @ TechStartups",
     text: "Shiva was instrumental in transforming our website into a powerful marketing tool. His attention to detail and ability to understand our brand is exceptional. We're thrilled with the results!",
     avatar: memojiAvatar1,
   },
   {
-    name: "Olivia Green",
+    name: "Alexa Puttelas",
     position: "Head of Design @ GreenLeaf",
     text: "Working with Shiva was a pleasure. His expertise in frontend development brought our designs to life in a way we never imagined. The website has exceeded our expectations.",
     avatar: memojiAvatar2,
   },
   {
-    name: "Daniel White",
+    name: "Neymer Junior",
     position: "CEO @ InnovateCo",
     text: "Shiva's ability to create seamless user experiences is unmatched. Our website has seen a significant increase in conversions since launching the new design. We couldn't be happier.",
     avatar: memojiAvatar3,
   },
   {
-    name: "Emily Carter",
+    name: "Unknown",
     position: "Product Manager @ GlobalTech",
     text: "Shiva is a true frontend wizard. He took our complex product and transformed it into an intuitive and engaging user interface. We're already seeing positive feedback from our customers.",
     avatar: memojiAvatar4,
   },
   {
-    name: "Michael Brown",
+    name: "L.Suarez",
     position: "Director of IT @ MegaCorp",
     text: "Shiav's work on our website has been nothing short of exceptional. He's a talented developer who is also a great communicator. We highly recommend him.",
     avatar: memojiAvatar5,
@@ -49,32 +50,36 @@ export const TestimonialsSection = () => {
           description="Don't just take my word for it.See what my clients have to say
         about my work."
         />
-        <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,trasparent)]">
-          <div className="flex flex-none gap-8">
-            {testimonials.map((testimonial) => (
-              <div
-                key={testimonial.name}
-                className=" max-w-xs md:max-w-md md:p-8 px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 md:mt-5 after:pointer-events-none bg-gray-800 rounded-3xl overflow-hidden z-0 after:z-10  relative after:content[''] after:absolute after:inset-0 after:outline-white/20 after:outline-2 after:rounded-3xl after:outline after:-outline-offset-2"
-              >
-                <div className="flex gap-4 items-center">
-                  <div className="size-14 bg-gray-700 inline-flex rounded-full items-center justify-center flex-shrink-0">
-                    <Image
-                      src={testimonial.avatar}
-                      alt={testimonial.name}
-                      className="max-h-full"
-                    />
-                  </div>
-                  <div>
-                    <div className="font-semibold">{testimonial.name}</div>
-                    <div className="text-sm text-white/40">
-                      {testimonial.position}
+        <div className="mt-12 lg:mt-20 flex overflow-x-clip py-4 [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,trasparent)]">
+          <div className="flex flex-none pr-8 gap-8 animate-move-left [animation-duration:65s] hover:[animation-play-state:paused]">
+            {[...new Array(2)].fill(0).map((_, index) => (
+              <Fragment key={index}>
+                {testimonials.map((testimonial) => (
+                  <div
+                    key={testimonial.name}
+                    className="hover:-rotate-3 transition duration-300  max-w-xs md:max-w-md md:p-8 px-8 pt-8 md:pt-12 md:px-10 lg:pt-10 lg:px-10 md:mt-5 after:pointer-events-none bg-gray-800 rounded-3xl overflow-hidden z-0 after:z-10  relative after:content[''] after:absolute after:inset-0 after:outline-white/20 after:outline-2 after:rounded-3xl after:outline after:-outline-offset-2"
+                  >
+                    <div className="flex gap-4 items-center">
+                      <div className="size-14 bg-gray-700 inline-flex rounded-full items-center justify-center flex-shrink-0">
+                        <Image
+                          src={testimonial.avatar}
+                          alt={testimonial.name}
+                          className="max-h-full"
+                        />
+                      </div>
+                      <div>
+                        <div className="font-semibold">{testimonial.name}</div>
+                        <div className="text-sm text-white/40">
+                          {testimonial.position}
+                        </div>
+                      </div>
                     </div>
+                    <p className="mt-4 mb-4 text-sm md:mt-6 md:text-base">
+                      {testimonial.text}
+                    </p>
                   </div>
-                </div>
-                <p className="mt-4 mb-4 text-sm md:mt-6 md:text-base">
-                  {testimonial.text}
-                </p>
-              </div>
+                ))}
+              </Fragment>
             ))}
           </div>
         </div>
