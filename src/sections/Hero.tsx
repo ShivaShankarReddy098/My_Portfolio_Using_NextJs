0a57f95aac39fea0e0992fd386ca4a96dc9323cd
ShivaShankarReddy098 <shankarreddyshiva83@gmail.com>
2024-10-09 14:44:14 +0530

    Added Gsap to Hero Section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,4 @@
+"use client";
 import memojiImage from "@/assets/images/memoji-computer.png";
 import Image from "next/image";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
@@ -6,9 +7,26 @@ import StarIcon from "@/assets/icons/star.svg";
 import { HeroOrbit } from "@/components/HeroOrbit";
 import Link from "next/link";
 import { ProjectsSection } from "./Projects";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
 export const HeroSection = () => {
+  useGSAP(() => {
+    // gsap.to("#img", { duration: 2, x: -40 });
+    gsap.from("#img", {
+      duration: 6,
+      y: -300,
+      rotation: 360,
+      ease: "bounce",
+    });
+    gsap.from("#heroH1", { duration: 10, x: 500, ease: "back" });
+    gsap.from("#heroPara", { duration: 15, x: -500, ease: "elastic" });
+    gsap.from("#btn1", { duration: 4, y: 700, ease: "back-in" });
+    gsap.from("#btn2", { duration: 5, y: 500, ease: "back-in" });
+  });
   return (
-    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+    <div className="py-32 md:py-48 lg:py-40 relative z-0 overflow-x-clip">
       <div
         className="absolute inset-0 -z-30 opacity-5"
         style={{
@@ -46,7 +64,12 @@ export const HeroSection = () => {
 
       <div className="container">
         <div className="flex flex-col items-center">
-          <Image src={memojiImage} className="size-[100px]" alt="heroImg" />
+          <Image
+            id="img"
+            src={memojiImage}
+            className="size-[100px]"
+            alt="heroImg"
+          />
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
             <div className="bg-green-400 size-2.5 rounded-full animate-pulse"></div>
             <div className="font-medium text-sm">
@@ -55,21 +78,33 @@ export const HeroSection = () => {
           </div>
         </div>
         <div className="max-w-lg mx-auto">
-          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+          <h1
+            id="heroH1"
+            className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide"
+          >
             Building Exceptional User Experience
           </h1>
-          <p className="mt-4 text-center text-white/60 md:text-lg">
+          <p
+            id="heroPara"
+            className="mt-4 text-center text-white/60 md:text-lg"
+          >
             I specialize in transforming designs into funtional,high-performing
             web applications.Let&apos;s discuss your next project.
           </p>
         </div>
         <div className="flex flex-col md:flex-row md:items-center justify-center items-center mt-8 gap-4">
-          <button className="inline-flex items-center z-10 gap-2 border border-white/15 px-6 h-12 rounded-xl">
+          <button
+            id="btn1"
+            className="inline-flex items-center z-10 gap-2 border border-white/15 px-6 h-12 rounded-xl"
+          >
             <span className="font-semibold">Explore My Work</span>
             <ArrowDown className="size-4" />
           </button>
 
-          <button className="inline-flex items-center z-10 gap-2 border border-white bg-white/90 text-green-900 h-12 px-6 rounded-xl">
+          <button
+            id="btn2"
+            className="inline-flex items-center z-10 gap-2 border border-white bg-white/90 text-green-900 h-12 px-6 rounded-xl"
+          >
             <span>🤝</span>
             <span className="font-semibold">Let&apos;s Connect</span>
           </button>
